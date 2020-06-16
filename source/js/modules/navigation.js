8b30bac045248820519ad2a6d873442126b5d69c
MariJunior <marinakaluzhnaya13@gmail.com>
2020-06-16 21:06:21 +0300

    Добавляет состояния интерактивным элементам. Доводит до ума работу меню. 🎉

--- source/js/modules/navigation.js
+++ source/js/modules/navigation.js
@@ -7,15 +7,26 @@ $(document).ready(function () {
   let mainMenu = document.querySelector('.navigation__wrap');
   let mainMenuToggler = document.querySelector('.navigation__burger');
 
+  const toggleMenu = function () {
+    mainMenu.classList.toggle('navigation__wrap--closed');
+    mainMenuToggler.classList.toggle('reset-button--opened');
+  }
+
   mainMenu.classList.add('navigation__wrap--closed');
 
-  mainMenuToggler.addEventListener('click', function() {
-    if (mainMenu.classList.contains('navigation__wrap--closed')) {
-      mainMenu.classList.remove('navigation__wrap--closed');
-      mainMenuToggler.classList.add('navigation__burger--opened');
-    } else {
-      mainMenu.classList.add('navigation__wrap--closed');
-      mainMenuToggler.classList.remove('navigation__burger--opened');
+  mainMenuToggler.addEventListener('click', function(evt) {
+    evt.stopPropagation();
+    toggleMenu();
+  });
+
+  document.addEventListener('click', function(evt) {
+    const target = evt.target;
+    const its_menu = target == mainMenu || mainMenu.contains(target);
+    const its_btnMenu = target == mainMenuToggler;
+    const menu_is_active = mainMenuToggler.classList.contains('reset-button--opened');
+
+    if (!its_menu && !its_btnMenu && menu_is_active) {
+      toggleMenu();
     }
   });
 });
